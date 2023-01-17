17d495e151cb2e0c9f514650b1f29705d865c7f1
Fredrik <fredrik.edstrom@synective.se>
2023-01-17 15:48:26 +0100

    Merged, and updated HTML

--- settings.py
+++ settings.py
@@ -1,15 +1,15 @@
 settings = {
     # This setting decides if we run model_train() or model_predict() -> train/test
-    "mode": "test",
+    "mode": "train",
 
     # File Paths:
     "api_folder": "application/static/api_folder/",
     "data_path": "data_jpg",
-    "test_path": "data_jpg\\test\\",
-    "model_weights": "nn_model/Model_results/model_90.pth",
-    "log_path": "nn_model/logs/",
+    "test_path": "nn_model\\data_jpg\\test\\",
+    "model_weights": "nn_model\\Model_results\\model_91.pth",
+
     # For Training:
-    "learning_rate": 0.000279,
+    "learning_rate": 0.00027,
     "weight_decay": 0.1,
     "batch_size": 20,
     "num_epochs": 100,
