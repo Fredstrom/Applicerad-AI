b29d5cb6621e11ac9da7a7b0057a020107c5c148
Fredrik <fredrik.edstrom@synective.se>
2023-01-22 16:47:09 +0100

    Added a README.md
    TODO: Change thte github links to each profile (and not just me :D)

--- settings.py
+++ settings.py
@@ -8,6 +8,7 @@ settings = {
     "test_path": "nn_model\\data_jpg\\test\\",
     "model_weights": "nn_model\\Model_results\\model_91.pth",
     "log_path": "nn_model/logs/",
+
     # For Training:
     "learning_rate": 0.00020,
     "weight_decay": 0.15,
